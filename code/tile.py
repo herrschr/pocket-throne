c1738b649bf92ae0406909199e2d2bd67239cca5
herrschr <sysherrsch0r@gmail.com>
2015-04-04 18:16:03 +0200

    tile.py: added get_image_path() method

--- code/tile.py
+++ code/tile.py
@@ -1,20 +1,29 @@
-import pygame
-
 class Tile:
     _id = -1
     pos_x = -1
     pos_y = -1
-    gui_x = -1
-    gui_y = -1
-    lndscp = "NULL"
+    landscape = None
+    _image_path = ""
 
     def __init__(self):
         pass
 
-    def initial(self,x,y,lndscp):
-        self.gui_x = TILES_SIZE * x
-        self.gui_y = TILES_SIZE * y
+    def __init__(self,x,y,landscape):
         self.pos_x = x
         self.pos_y = y
-        self.lndscp = lndscp
-        
+        self.landscape = landscape
+        self._image_path = get_image_path()
+
+    def get_image_path(self):
+        if (self.landscape == "G"):
+            return "tile_grass.png"
+        elif (self.landscape == "D"):
+            return "tile_dirt.png"
+        elif (self.landscape == "W"):
+            return "tile_water.png"
+        elif (self.landscape == "M"):
+            return "tile_mointain.png"
+        elif (self.landscape == "F"):
+            return "tile_forest.png"
+        else:
+            return None
