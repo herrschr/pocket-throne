02b29dca8406f4cb47926999ef366e5dd3bfd503
herrschr <sysherrsch0r@gmail.com>
2015-04-04 18:41:30 +0200

    tilemap.py: add_tile(tile) method added

--- code/tilemap.py
+++ code/tilemap.py
@@ -1,8 +1,16 @@
 class TileMap:
-    TILES_SIZE = 40
-    _id = -1
-    tiles = []
-    tiles_at = {}
-    
-    def __init__(self):
-        pass
+	TILES_SIZE = 40
+	_id = -1
+	tiles = []
+	tiles_at = {}
+
+	def __init__(self):
+		pass
+
+	def add_tile(self, tile_to_add):
+		tiles.add(tile_to_add)
+		pos_x = tile.pos_x
+		pos_y = tile.pos_y
+		tiles_at[pos_x, pos_y] = tile_to_add
+
+
