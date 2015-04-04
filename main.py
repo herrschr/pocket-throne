ed62a67e68496b58be630f6b19853d2b5f0568a8
herrschr <sysherrsch0r@gmail.com>
2015-04-04 23:04:06 +0200

    code package renamed in core; game classes import in __init__.py

--- main.py
+++ main.py
@@ -1,62 +1,57 @@
-from code.mapmanager import MapManager
-from code.tile import Tile
-from code.tilemap import TileMap
-from code.unitmanager import UnitManager
+# IMPORTS
+# user lib & system lib import
 import pygame
 
-#Pygame-Init
+# manager imports
+from core.managers.mapmanager import MapManager
+from core.managers.unitmanager import UnitManager
 
-screen = None
+# entity imports
+from core.entities.tile import Tile
+from core.entities.tilemap import TileMap
 
+# METHODS
+# catch events from game loop and react to them
 def events():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            pos = pygame.mouse.get_pos()
-            print pos
-
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			sys.exit()
+		elif event.type == pygame.MOUSEBUTTONDOWN:
+			pos = pygame.mouse.get_pos()
+			print pos
+# initialize pygame & pygame screen
 def pygame_init():
-    global screen
-    print screen
-    pygame.init()
-    screen = pygame.display.set_mode((120, 120))
-    screen.fill((200, 200, 200))
-    pygame.display.update()
+	global screen
+	pygame.init()
+	screen = pygame.display.set_mode((120, 120))
+	screen.fill((200, 200, 200))
+	pygame.display.update()
 
+# INIT scripts
+# initialize pygame
+screen = None
 pygame_init()
 
-# making fake map
+# making fake map class
 _map = TileMap()
 _map.name = "Test Map"
 
-tile_0_0 = Tile(0, 0, "W")
-tile_1_0 = Tile(1, 0, "W")
-tile_2_0 = Tile(2, 0, "W")
-tile_0_1 = Tile(0, 1, "D")
-tile_1_1 = Tile(1, 1, "D")
-tile_2_1 = Tile(2, 1, "D")
-tile_0_2 = Tile(0, 2, "G")
-tile_1_2 = Tile(1, 2, "G")
-tile_2_2 = Tile(2, 2, "G")
-
-_map.add_tile(tile_0_0)
-_map.add_tile(tile_1_0)
-_map.add_tile(tile_2_0)
-_map.add_tile(tile_0_1)
-_map.add_tile(tile_1_1)
-_map.add_tile(tile_2_1)
-_map.add_tile(tile_0_2)
-_map.add_tile(tile_1_2)
-_map.add_tile(tile_2_2)
+# manually create tiles and add them to map
+_map.add_tile(Tile(0, 0, "W"))
+_map.add_tile(Tile(1, 0, "W")
+_map.add_tile(Tile(2, 0, "W"))
+_map.add_tile(Tile(0, 1, "D"))
+_map.add_tile(Tile(1, 1, "D"))
+_map.add_tile(Tile(2, 1, "D"))
+_map.add_tile(Tile(0, 2, "G"))
+_map.add_tile(Tile(1, 2, "G"))
+_map.add_tile(Tile(2, 2, "G"))
 
 # Manager initialization
 _mapmanager = MapManager(_map)
 _unitmanager = UnitManager("base")
 
-print "DEBUG MAP: " + str(_mapmanager._map.tiles_at)
-
-# game loop
+# GAME LOOP
 while 1==1:
 	# draw tiles
 	for tile in _mapmanager._map.tiles:
@@ -64,6 +59,8 @@ while 1==1:
 		gui_position = _mapmanager.pos_to_gui((tile.pos_x, tile.pos_y))
 		image = pygame.image.load(full_img_path)
 		screen.blit(image, gui_position)
+	# catch mouse event
 	events()
+	# redraw display
 	pygame.display.flip()
 
