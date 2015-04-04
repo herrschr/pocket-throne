f3d391fd1f02e08e232e70bf1231e5ed68f4f909
hoerns <eisbaer1024@gmail.com>
2015-04-04 16:57:07 +0200

    Mapmanager erweitert

--- mapmanager.py
+++ mapmanager.py
@@ -1,14 +1,30 @@
-class MapManager:
-    pass
+import pygame
 
+class MapManager:
+    _tilesize = 40
+    _map = None
+    
+    def __init__(self, tilemap):
+        if tilemap == None:
+            return None
+        self._map = tilemap
+
+    def pos_to_gui(self,x,y):
+        self.gui_x = x * self._tilesize
+        self.gui_y = y * self._tilesize
+    
     def spawn_unit(self):
         pass
 
     def move_unit(self):
         pass
 
-    def spawn_building(self):
-        pass
+    def spawn_building(self, building):
+        if building == None:
+            pass
+        else:
+            image = pygame.image.load(building.image)
+            screen.blit(image, (self.gui_x,self.gui_y))
 
     def spawn_landscape(self):
         if self.lndscp == "NULL":
