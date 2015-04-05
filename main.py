1ad67a3a4d5c8e43da8897921ee20f576357d572
herrschr <sysherrsch0r@gmail.com>
2015-04-05 18:25:12 +0200

    main.py: fully replaced pygame with pygame_sdl2

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # import libraries
-import sys
-import pygame_sdl2
-pygame_sdl2.import_as_pygame()
-import pygame
+import os, sys
+lib_path = os.path.abspath(os.path.join(".", "lib", "unix86"))
+sys.path.append(lib_path)
+from lib.unix86.pygame_sdl2 import pygame_sdl2
+# pygame_sdl2.import_as_pygame()
 
 # import whole core package
 from core import *
@@ -11,9 +12,9 @@ from core import *
 def pygame_init():
 	global screen
 	pygame_sdl2.init()
-	screen = pygame.display.set_mode((500, 500))
+	screen = pygame_sdl2.display.set_mode((500, 500))
 	screen.fill((255, 255, 255))
-	pygame.display.update()
+	pygame_sdl2.display.update()
 
 # INIT scripts
 # initialize pygame
@@ -39,16 +40,16 @@ _unitmanager.spawn_unit_at(1, "archer", (10, 6))
 
 # catch events from game loop and react to them
 def events():
-	for event in pygame.event.get():
+	for event in pygame_sdl2.event.get():
 		# on exit
-		if event.type == pygame.QUIT:
+		if event.type == pygame_sdl2.QUIT:
 			sys.exit()
 		# on left mouse press
-		elif event.type == pygame.MOUSEBUTTONDOWN:
-			gui_pos = pygame.mouse.get_pos()
+		elif event.type == pygame_sdl2.MOUSEBUTTONDOWN:
+			gui_pos = pygame_sdl2.mouse.get_pos()
 			tile_pos = MapManager.gui_to_pos(gui_pos)
 			_mapmanager.select_tile_at(tile_pos)
-			print "selected tile: " + str(tile_pos)
+			print ("selected tile: " + str(tile_pos))
 
 # GAME LOOP
 while True:
@@ -56,21 +57,21 @@ while True:
 	for tile in _map.tiles:
 		full_img_path = FileManager.image_path() + tile._image_path
 		gui_position = MapManager.pos_to_gui((tile.pos_x, tile.pos_y))
-		image = pygame.image.load(full_img_path)
+		image = pygame_sdl2.image.load(full_img_path)
 		screen.blit(image, gui_position)
 
 	# draw selected tile overlay
 	if (_mapmanager.has_selected_tile):
 		selected_img_path = FileManager.image_path() + "tile_selected.png"
 		selected_gui_pos = MapManager.pos_to_gui(_mapmanager.selected.get_position())
-		selected_img = pygame.image.load(selected_img_path)
+		selected_img = pygame_sdl2.image.load(selected_img_path)
 		screen.blit(selected_img, selected_gui_pos)
 
 	# draw units
 	for unit in _unitmanager._units:
 		full_img_path = FileManager.image_path() + unit.image_path
 		gui_position = MapManager.pos_to_gui((unit.pos_x, unit.pos_y))
-		image = pygame.image.load(full_img_path)
+		image = pygame_sdl2.image.load(full_img_path)
 		screen.blit(image, gui_position)
 
 	# TODO: draw buildings
@@ -78,4 +79,4 @@ while True:
 	# catch mouse events
 	events()
 	# redraw display
-	pygame.display.flip()
+	pygame_sdl2.display.flip()
