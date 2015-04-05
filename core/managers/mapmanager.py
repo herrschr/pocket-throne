cd6784e234c910fb888eacd15f870a536362f651
herrschr <sysherrsch0r@gmail.com>
2015-04-05 04:16:26 +0200

    added selected tile selection & drawing

--- core/managers/mapmanager.py
+++ core/managers/mapmanager.py
@@ -3,6 +3,8 @@ from core.entities.gamestate import GameState
 class MapManager:
 	_tilesize = 40
 	_map = None
+	has_selected_tile = False
+	selected = None
 
 	def __init__(self, tilemap):
 		# set self._map
@@ -12,8 +14,23 @@ class MapManager:
 		# update map in gamestate
 		GameState.set_actual_map(self._map)
 
+	# set tile at given position as selected, accepts two int values for position
+	def select_tile_at(self, pos_x, pos_y):
+		return self.select_tile_at((pos_x, pos_y))
+
+	# set tile at given position tuple as selected
+	def select_tile_at(self, (pos_x, pos_y)):
+		self.selected = self._map.get_tile_at((pos_x, pos_y))
+		self.has_selected_tile = True
+		return self.selected
+
 	# return the GUI position for a tile
-	def pos_to_gui(self,(x,y)):
+	def pos_to_gui(self,(x, y)):
 		gui_x = x * self._tilesize
 		gui_y = y * self._tilesize
 		return (gui_x, gui_y)
+
+	def gui_to_pos(self, (x, y)):
+		pos_x = int(x / self._tilesize)
+		pos_y = int(y / self._tilesize)
+		return (pos_x, pos_y)
--- main.py
+++ main.py
@@ -1,19 +1,9 @@
-# IMPORTS
 # import libraries
 import sys
 import pygame
+
 # import whole core package
 from core import *
-
-# METHODS
-# catch events from game loop and react to them
-def events():
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			sys.exit()
-		elif event.type == pygame.MOUSEBUTTONDOWN:
-			pos = pygame.mouse.get_pos()
-			print pos
 
 # initialize pygame & pygame screen
 def pygame_init():
@@ -46,14 +36,34 @@ _ingamemanager.add_new_player("Player 2", (0, 0, 255))
 _unitmanager.spawn_unit_at(1, "swordsman", (2, 9))
 _unitmanager.spawn_unit_at(1, "swordsman", (10, 6))
 
+# catch events from game loop and react to them
+def events():
+	for event in pygame.event.get():
+		# on exit
+		if event.type == pygame.QUIT:
+			sys.exit()
+		# on left mouse press
+		elif event.type == pygame.MOUSEBUTTONDOWN:
+			gui_pos = pygame.mouse.get_pos()
+			tile_pos = _mapmanager.gui_to_pos(gui_pos)
+			_mapmanager.select_tile_at(tile_pos)
+			print "selected tile: " + str(tile_pos)
+
 # GAME LOOP
 while 1==1:
 	# draw tiles
 	for tile in _map.tiles:
-		full_img_path = "img/" + tile._image_path
+		full_img_path = FileManager.image_path() + tile._image_path
 		gui_position = _mapmanager.pos_to_gui((tile.pos_x, tile.pos_y))
 		image = pygame.image.load(full_img_path)
 		screen.blit(image, gui_position)
+
+	# draw selected tile overlay
+	if (_mapmanager.has_selected_tile):
+		selected_img_path = FileManager.image_path() + "tile_selected.png"
+		selected_gui_pos = _mapmanager.pos_to_gui(_mapmanager.selected.get_position())
+		selected_img = pygame.image.load(selected_img_path)
+		screen.blit(selected_img, selected_gui_pos)
 
 	# draw units
 	for unit in _unitmanager._units:
