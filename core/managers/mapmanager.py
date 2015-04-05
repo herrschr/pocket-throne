960c9fce7d0252da429a986ab3fd0b0d99fbfc68
herrschr <sysherrsch0r@gmail.com>
2015-04-05 03:01:57 +0200

    mapmanager.py: cleaned up

--- core/managers/mapmanager.py
+++ core/managers/mapmanager.py
@@ -1,4 +1,3 @@
-import pygame
 from core.entities.gamestate import GameState
 
 class MapManager:
@@ -10,22 +9,11 @@ class MapManager:
 		if tilemap == None:
 			return None
 		self._map = tilemap
+		# update map in gamestate
 		GameState.set_actual_map(self._map)
 
+	# return the GUI position for a tile
 	def pos_to_gui(self,(x,y)):
 		gui_x = x * self._tilesize
 		gui_y = y * self._tilesize
 		return (gui_x, gui_y)
-
-	def spawn_unit(self,x,y):
-		pass
-
-	def move_unit(self):
-		pass
-
-	def spawn_building(self, building,x,y):
-		if building == None:
-			pass
-		else:
-			image = pygame.image.load(building.image)
-			screen.blit(image, pos_to_gui(x,y))
