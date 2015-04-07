e577b88f3f20753a1bab197bb9fa3dcda96e926c
herrschr <sysherrsch0r@gmail.com>
2015-04-07 14:43:07 +0200

    labelwidget.py: __repr__() added; set_text() updated with redraw trigger

--- core/gui/labelwidget.py
+++ core/gui/labelwidget.py
@@ -1,20 +1,29 @@
 from core.lib.posix.pygame_sdl2.font import Font
+from core.gui.widget import Widget
 
 class LabelWidget(Widget):
+	color = (0, 0, 0)
+
 	def __init__(self, eventmanager):
 		Widget.__init__(self, eventmanager)
-		self.color(200, 200, 200)
+		# self.color(200, 200, 200)
 		self.font = Font(None, 30)
-        		self.__text = text
-		self.image = self.font.render( self.__text, 1, self.color)
+		self.__text = "Loading ..."
+		self.image = self.font.render(self.__text, 1, self.color)
 		self.rect  = self.image.get_rect()
 
-	def set_text(self, text):
-		self.__text = text
-		self._redraw = True
+	def set_text(self, to_set):
+		self.__text = to_set
+		self.trigger_redraw()
 
-	def update(self)
-		if not self._redraw:
+	def get_text(self):
+		return self.__text
+
+	def update(self):
+		if not self.dirty:
 			return
 		self.image = self.font.render(self.__text, 1, self.color)
-		self._redraw = False
+		self.dirty = False
+
+	def __repr__(self):
+		return "<LabelWidget panel=" + str(self.parent.anchor) + " text=" + self.__text + ">"
