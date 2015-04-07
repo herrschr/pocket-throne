5529a1e6a47a6e1f7926188ee0e7d8146722f586
herrschr <sysherrsch0r@gmail.com>
2015-04-07 14:41:44 +0200

    panel.py: set widgets layout attribute on addition to panel

--- core/gui/panel.py
+++ core/gui/panel.py
@@ -1,16 +1,13 @@
 from core.gui.widget import Widget
 from core.gui import *
+
 class Panel(Widget):
 	widgets = []
 	anchor = None
 	align = None
 	anchor = PANEL_ANCHOR_NOT_SET
-	layout = {
-		"left": -1,
-		"top": -1,
-		"width": -1,
-		"height": -1
-	}
+	padding = 8
+	color = (198, 172, 136)
 
 	def __init__(self, eventmanager, panel_anchor):
 		Widget.__init__(self, eventmanager)
@@ -19,17 +16,20 @@ class Panel(Widget):
 		# top or bottom panel: horizontal alignment
 		if panel_anchor == PANEL_ANCHOR_TOP or panel_anchor == PANEL_ANCHOR_BOTTOM:
 			self.align = PANEL_ALIGN_HORIZONTAL
+			self.layout["left"] = 0
+			self.layout["height"] = 64
 		# side panels: vertical alignment
 		elif panel_anchor == PANEL_ANCHOR_LEFT or panel_anchor == PANEL_ANCHOR_RIGHT:
 	 		self.align = PANEL_ALIGN_VERTICAL
 
 	def add_widget(self, to_add):
+		widget_num = len(self.widgets)
+		if self.align == PANEL_ALIGN_HORIZONTAL:
+			# add padding to left and top
+			to_add.layout["left"] = (self.layout["left"] + self.padding)
+			to_add.layout["top"] = (self.layout["top"] + self.padding)
+		to_add.parent = self
 		self.widgets.append(to_add)
 
-	def update(self):
-		if not self.dirty:
-			return
-		# draw panel
-
 	def __repr__(self):
-		return "<Panel anchor=" + str(self.anchor) + ">"
+		return "<Panel anchor=" + str(self.anchor) + " widgets=" + str(len(self.widgets))+ ">"
