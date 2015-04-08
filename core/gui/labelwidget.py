a20ad72eb8a607df2c78b878282fc0fc865061eb
herrschr <sysherrsch0r@gmail.com>
2015-04-08 07:47:04 +0200

    labelwidget.py: _update_size() added

--- core/gui/labelwidget.py
+++ core/gui/labelwidget.py
@@ -1,4 +1,4 @@
-from core.lib.posix.pygame_sdl2.font import Font
+from pygame_sdl2.font import Font
 from core.gui.widget import Widget
 
 class LabelWidget(Widget):
@@ -6,18 +6,26 @@ class LabelWidget(Widget):
 
 	def __init__(self, eventmanager):
 		Widget.__init__(self, eventmanager)
-		# self.color(200, 200, 200)
+		# render Widget's image
 		self.font = Font(None, 30)
 		self.__text = "Loading ..."
 		self.image = self.font.render(self.__text, 1, self.color)
-		self.rect  = self.image.get_rect()
+		# update size; trigger reload
+		self._update_size()
 
 	def set_text(self, to_set):
+		# change text
 		self.__text = to_set
+		# set new widget size
+		self._update_size()
 		self.trigger_redraw()
 
 	def get_text(self):
 		return self.__text
+
+	def _update_size(self):
+		rect = self.image.get_rect()
+		self.set_layout((None, None, rect.width, rect.height))
 
 	def update(self):
 		if not self.dirty:
@@ -26,4 +34,4 @@ class LabelWidget(Widget):
 		self.dirty = False
 
 	def __repr__(self):
-		return "<LabelWidget panel=" + str(self.parent.anchor) + " text=" + self.__text + ">"
+		return "<LabelWidget panel=" + str(self.parent.anchor) + " text=" + self.__text + " layout=" + str(self.get_layout()) + ">"
