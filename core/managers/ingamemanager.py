ffa2bbe80026dd3da3ca4606019e4236ec922ed6
herrschr <sysherrsch0r@gmail.com>
2015-04-08 07:35:07 +0200

    ingamemanager.py: ++ change_actual_player(); Next Player on [Space] press

--- core/managers/ingamemanager.py
+++ core/managers/ingamemanager.py
@@ -1,6 +1,9 @@
 from core.entities.player import Player
 from core.entities.event import *
 
+# import pygame key constants
+from pygame_sdl2 import *
+
 # Manager class for players & turn methods
 class IngameManager:
 	_tag = "IngameManager: "
@@ -25,10 +28,10 @@ class IngameManager:
 		self.add_player(nature)
 
 	def start_game(self):
-		# set turn and player
+		# set turn=0 and player=1
 		self.actual_turn = 0
-		self.actual_player_num = 1
-		self.actual_player = self.players[self.actual_player_num]
+		actual_player_num = 1
+		self.change_actual_player(1)
 
 	# add a player by class
 	def add_player(self, player):
@@ -37,7 +40,7 @@ class IngameManager:
 		new_player.num = self.next_player_number()
 		self.players.append(new_player)
 		# debug: log player addition
-		print self._tag + "added player name=" + new_player.name + " on number " + str(new_player.num)
+		print (self._tag + "added player name=" + new_player.name + " on number " + str(new_player.num))
 
 	# add a new player by name and color
 	def add_new_player(self, player_name, (r, g, b)):
@@ -46,33 +49,40 @@ class IngameManager:
 		new_player.color = (r, g, b)
 		self.add_player(new_player)
 
+	def change_actual_player(self, player_num):
+		# update player
+		self.actual_player_num = player_num
+		self.actual_player = self.players[player_num]
+		# fire NextOneEvent
+		ev_nextone = NextOneEvent(self.actual_player)
+		self._eventmgr.fire(ev_nextone)
+
 	# internal: end turn and start a next one
 	def next_turn(self):
 		# next turn
 		self.actual_turn += 1
-		# player one starts
-		self.actual_player_num = 1
-		self.actual_player = self.players[self.actual_player_num]
 		# trigger NextTurnEvent
-		self._eventmgr.post(NextTurnEvent(self.actual_turn))
+		ev_nextturn = NextTurnEvent(self.actual_turn)
+		self._eventmgr.fire(ev_nextturn)
+		# player one starts
+		actual_player_num = 1
+		self.change_actual_player(actual_player_num)
 
 	# internal: end turn of the actual player and switch to next one
 	def next_one(self):
 		# next player
-		self.actual_player_num += 1
-		self.actual_player = self.players[self.actual_player_num]
-		# trigger NextOneEvent
-		self._eventmgr.post(NextOneEvent(self.actual_player))
+		actual_player_num = self.actual_player_num + 1
+		self.change_actual_player(actual_player_num)
 
 	# switch player or update turn, "End Turn" button beheaviour
 	def forward(self):
-		player_count = len(self.players)
+		player_count = len(self.players) -1
 		# player isn't the last one
 		if player_count - self.actual_player_num > 0:
-			next_one()
+			self.next_one()
 		# player is the last one this turn
 		elif player_count - self.actual_player_num  == 0:
-			next_turn()
+			self.next_turn()
 
 	# internal: get next player number
 	def next_player_number(self):
@@ -80,6 +90,15 @@ class IngameManager:
 		return self._last_player_id
 
 	def on_event(self, event):
+		if isinstance(event, GameStartedEvent):
+			self.start_game()
+
 		if isinstance(event, MouseClickedEvent):
 			pass
+
+		if isinstance(event, KeyPressedEvent):
+			key = event.key
+			if key == K_SPACE:
+				self.forward()
+
 
