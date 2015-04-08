1842418418dae49a3687d37d702ca8f108fcada3
herrschr <sysherrsch0r@gmail.com>
2015-04-08 07:50:29 +0200

    guimanager.py: added panel id creation

--- core/managers/guimanager.py
+++ core/managers/guimanager.py
@@ -1,11 +1,13 @@
 from core.gui.panel import Panel
 from core.gui.labelwidget import LabelWidget
+from core.gui.multilinelabelwidget import MultiLineLabelWidget
 from core.gui import *
 from core.entities.event import *
 
 class GuiManager:
 	panels = []
 	panel_at = {}
+	_last_panel_id = -1
 
 	def __init__(self, eventmanager):
 		print ("guimgr: init with " + str(eventmanager))
@@ -13,14 +15,20 @@ class GuiManager:
 		self._eventmgr.register_listener(self)
 		self.gamestate = GAMESTATE_LOADING
 
+	# returns a new gui panel id
+	def next_panel_id(self):
+		self._last_panel_id += 1
+		return self._last_panel_id
+
 	def add_panel(self, panel):
-		# add pannel to collections
+		# add panel to collections
 		anchor = panel.anchor
-		self.panel_at[anchor] = panel
+		panel._id = self.next_panel_id()
+		self.panel_at[panel.anchor] = panel
 		self.panels = panel
 		# fire GuiPanelAddedEvent
 		ev_panel_added = GuiPanelAddedEvent(anchor, panel)
-		self._eventmgr.post(ev_panel_added)
+		self._eventmgr.fire(ev_panel_added)
 
 	def click_in_gui(self, (click_x, click_y)):
 		for panel in self.panels:
@@ -36,14 +44,26 @@ class GuiManager:
 			self.gamestate = GAMESTATE_INGAME
 			# add bottom panel
 			bottom_panel = Panel(self._eventmgr, PANEL_ANCHOR_BOTTOM)
-			bottom_label = LabelWidget(self._eventmgr)
-			bottom_label.set_text("Hi!")
-			bottom_panel.add_widget(bottom_label)
+			# add heading label
+			h_label = MultiLineLabelWidget(self._eventmgr)
+			h_label.set_line(0, "No Selection..")
+			# add data label
+			# data_label = LabelWidget(self._eventmgr)
+			# data_label.set_text("nodata")
+			# add them to panel and add panel to GuiManager
+			bottom_panel.add_widget(h_label)
+			# bottom_panel.add_widget(data_label)
 			self.add_panel(bottom_panel)
 
-		# when a unit is selected, show unit's name on
+		# when a unit is selected, show unit's name on label1
 		if isinstance(event, UnitSelectedEvent):
 			selected_unit = event.unit
-			label1 = self.panel_at[PANEL_ANCHOR_BOTTOM].widgets[0]
-			label1.set_text("Unit: " + selected_unit.name)
+			selected_label = self.panel_at[PANEL_ANCHOR_BOTTOM].widgets[0]
+			selected_label.set_line(1, "Unit: " + selected_unit.name)
+			selected_label.set_line(2, "HP: " + str(selected_unit.hp) + " | MP: " + str(selected_unit.mp))
 
+		# when a unit is unselected, hide label1
+		if isinstance(event, UnitUnselectedEvent):
+			selected_label = self.panel_at[PANEL_ANCHOR_BOTTOM].widgets[0]
+			selected_label.set_line(1, "")
+			selected_label.set_line(2, "")
