43a932be2978257e8be433a4dbf7ce18b42c8eb6
herrschr <sysherrsch0r@gmail.com>
2015-05-09 23:22:58 +0200

    entities: fixed auto-import and __all__

---
+++ core/entities/__init__.py
@@ -0,0 +1,8 @@
+__all__ = ["building", "event", "player", "tile", "tilemap", "unit"]
+
+from building import Building
+from event import *
+from player import Player
+from tile import Tile
+from tilemap import TileMap
+from unit import Unit, Weapon
