657d784d23cd681a92c7aff7a32a3d5be7289330
herrschr <sysherrsch0r@gmail.com>
2015-08-03 17:58:16 +0200

    mapmanager: make terrain bridges

--- core/managers/mapmanager.py
+++ core/managers/mapmanager.py
@@ -1,35 +1,49 @@
 from core.tools.maploader import MapLoader
 from core.managers.filemanager import FileManager
+from core.managers.eventmanager import EventManager
 from core.entities.event import *
+from core.lang.unique import Unique
 
-class MapManager:
+class MapManager(Unique):
 	_tilesize = 40
 	_map = None
+	_mod = "base"
 	has_selected_tile = False
-	selected = None
+	selected_tile = None
 
-	def __init__(self, eventmanager, map_name=None, mod="base"):
+	def __init__(self, map_name=None, mod="base"):
 		# register in EventManager
-		self._eventmgr = eventmanager
-		self._eventmgr.register_listener(self)
+		EventManager.register_listener(self)
 		# set self._map
+		self._mod = mod
 		if map_name == None:
 			return
 		else:
 			self.load_map(map_name)
 
 	def load_map(self, map_name):
-		tilemap = MapLoader(map_name).get_map()
+		tilemap = MapLoader(map_name, mod=self._mod).get_map()
 		self._map = self.postload_map(tilemap)
-		self._eventmgr.fire(MapLoadedEvent(self._map))
+		EventManager.fire(MapLoadedEvent(self._map))
 
 	def postload_map(self, tilemap):
 		# set terrain bridges
 		tilemap.initialize_neighbortiles()
 		for tile in tilemap.tiles:
+			n_north = tile.get_neighbor("N")
+			n_south = tile.get_neighbor("S")
+			n_west = tile.get_neighbor("W")
+			n_east = tile.get_neighbor("E")
+			# WATER
 			if tile.get_landscape() == "W":
-				if tile._neighbor_north == "G" or tile._neighbor_north == "M":
+				if n_north != "W":
 					tile.image_override = "tile_water_ontop_grass.png"
+			# SNOW
+			if tile.get_landscape() == "S":
+				if n_north == "G" or n_north == "F" or n_north == "M":
+					tile.image_override = "tile_snow_north_grass.png"
+				elif n_south == "G" or n_south == "F" or n_south == "M":
+					tile.image_override = "tile_snow_south_grass.png"
 		return tilemap
 
 	def get_loaded_map(self):
@@ -40,25 +54,26 @@ class MapManager:
 
 	# set tile at given position tuple as selected
 	def select_tile_at(self, (pos_x, pos_y)):
-		self.selected = self._map.get_tile_at((pos_x, pos_y))
-		if self.selected == None:
+		self.selected_tile = self._map.get_tile_at((pos_x, pos_y))
+		if self.selected_tile == None:
 			self.has_selected_tile = False
 			return None
 		self.has_selected_tile = True
-		self._eventmgr.fire(TileSelectedEvent(self.selected))
-		return self.selected
+		EventManager.fire(TileUnselectedEvent())
+		EventManager.fire(TileSelectedEvent(self.selected_tile, (pos_x, pos_y)))
+		return self.selected_tile
 
 	# revert tile selection
 	def unselect_tile(self):
 		self.has_selected_tile = False
 		self.selected = None
-		self._eventmgr.fire(TileUnselectedEvent())
+		EventManager.fire(TileUnselectedEvent())
 
 	# translates a TileMap grid position into display size
 	@classmethod
 	def pos_to_gui(self,(x, y)):
 		gui_x = x * self._tilesize
-		gui_y = y * self._tilesize
+		gui_y = self._map.size_y - y * self._tilesize
 		return (gui_x, gui_y)
 
 	# translates a position into the TileMap grid
@@ -73,7 +88,7 @@ class MapManager:
 		if isinstance(event, MouseClickedEvent):
 			# get selected tile
 			grid_pos = self.gui_to_pos(event.pos)
-			selected_tile = self._map.get_tile_at(grid_pos)
+			# self.selected_tile = self._map.get_tile_at(grid_pos)
 			# fire TileSelectedEvent
-			ev_tile_selected = TileSelectedEvent(selected_tile, grid_pos)
-			self._eventmgr.fire(ev_tile_selected)
+			# ev_tile_selected = TileSelectedEvent(self.selected_tile, grid_pos)
+			# EventManager.fire(ev_tile_selected)
