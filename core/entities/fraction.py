777d153cbc14287b16a83985f4e1a907823dbc97
herrschr <sysherrsch0r@gmail.com>
2015-08-03 17:44:03 +0200

    added fraction entity

---
+++ core/entities/fraction.py
@@ -0,0 +1,12 @@
+class Fraction:
+	name = ""
+	name_de = ""
+
+	city_prefixes = None
+	city_postfixes = None
+
+	def __init__(self):
+		pass
+
+	def __repr__(self):
+		return "<Fraction name=" + self.name + ">"
