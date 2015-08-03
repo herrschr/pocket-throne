7aa2297ffe8d87ba4837b20e55140a20d8cce8e6
herrschr <sysherrsch0r@gmail.com>
2015-08-03 17:43:23 +0200

    made eventmanager a static class

--- core/managers/eventmanager.py
+++ core/managers/eventmanager.py
@@ -1,21 +1,24 @@
 from core.entities.event import *
+from core.lang.borg import Borg
+from weakref import WeakKeyDictionary
 
 class EventManager:
-	# this object is responsible for coordinating most communication
-	def __init__(self):
-		from weakref import WeakKeyDictionary
-		self.listeners = WeakKeyDictionary()
-		self.eventQueue= []
+	_tag = "[EventManager] "
+	listeners = WeakKeyDictionary()
+	eventQueue= []
 
+	@classmethod
 	def register_listener(self, listener):
 		self.listeners[listener] = 1
 
+	@classmethod
 	def unregister_listener( self, listener ):
 		if listener in self.listeners:
 			del self.listeners[listener]
 
+	@classmethod
 	def fire(self, event):
 		if not isinstance(event, TickEvent):
-			print("EventManager: Message: " + event.name)
+			print(self._tag + "Message: " + event.name)
 		for listener in list(self.listeners):
 			listener.on_event(event)
