d04b65951ddc9abecf47e296f6db30e0a7c3715c
herrschr <sysherrsch0r@gmail.com>
2015-08-03 17:55:52 +0200

    unitmanager: implemented unit attacks

--- core/managers/unitmanager.py
+++ core/managers/unitmanager.py
@@ -1,6 +1,7 @@
 import os
 import json
 import copy
+from random import randrange
 
 from core.entities.unit import Unit, Weapon
 from core.entities.tile import Tile
@@ -12,22 +13,28 @@ from core.entities.event import *
 from core.tools.unitmovementhelper import UnitMovementHelper
 
 class UnitManager:
-	_tag = "UnitManager: "
+	_tag = "[UnitManager] "
+
+	# blueprint & unit list
 	_skeletons = {}
 	_units = []
-	_map = None
 	_last_unit_id = -1
 
+	_map = None
 	_actual_player = None
-	_selected = None
 
-	def __init__(self, eventmanager, tilemap, mod="base"):
+	# selected unit cache
+	_selected = None
+	_selected_moves = []
+	_selected_attacks = []
+
+	_selected_city = None
+
+	def __init__(self, tilemap, mod="base"):
 		# register in EventManager
-		self._eventmgr = eventmanager
-		self._eventmgr.register_listener(self)
-		# ignore the mod name, no active modding system
+		EventManager.register_listener(self)
 		# load all unit blueprints
-		self.load_unit_skeletons("base")
+		self.load_unit_skeletons(mod)
 		# spawn units from map file
 		self._map = tilemap
 
@@ -36,6 +43,7 @@ class UnitManager:
 		self._last_unit_id += 1
 		return self._last_unit_id
 
+	# get the unit on grid position (at_x, at_y), else return None
 	def get_unit_at(self, (at_x, at_y), for_specific_player=None):
 		for unit in self._units:
 			unit_x = int(unit.pos_x)
@@ -47,18 +55,37 @@ class UnitManager:
 					return unit
 		return None
 
+	# select a unit and fire UnitSelectedEvent
+	def select_unit(self, unit):
+		self._selected = unit
+		# cache & add possible moves and attacks to event
+		self._selected_moves = self.get_possible_moves(unit)
+		self._selected_attacks = self.get_possible_attacks(unit)
+		# fire UnitSelectedEvent
+		ev_selected_unit = UnitSelectedEvent(unit, moves=self._selected_moves, attacks=self._selected_attacks)
+		EventManager.fire(ev_selected_unit)
+
+	def unselect_unit(self):
+		self._selected = None
+		self._selected_moves = []
+		self._selected_attacks = []
+
 	# load unit skeletons from mods/<mod_name>/units/*.json
 	def load_unit_skeletons(self, mod_name):
 		unit_folder_path = FileManager.mod_path() + mod_name + "/units/"
 		for file in os.listdir(unit_folder_path):
 			if file.endswith(".json"):
-				# load json file
+				# open file and read content
 				unit_basename = file.split(".")[0]
 				unit_file_path = unit_folder_path + file
-				unit_json = json.loads(FileManager.read_file(unit_file_path))
-				# load skeleton Unit from json and add it skeleton list
-				unit = self.load_unit_skeleton(unit_json)
-				self._skeletons[unit_basename] = unit
+				unit_filecontent = FileManager.read_file(unit_file_path)
+				# load json content to class
+				if unit_filecontent != "":
+					unit_json = json.loads(unit_filecontent)
+					# load skeleton Unit from json and add it skeleton list
+					unit = self.load_unit_skeleton(unit_json)
+					unit._basename = unit_basename
+					self._skeletons[unit_basename] = unit
 		self.print_skeletons()
 
 	# fill a Unit skeleton with a json dict
@@ -71,6 +98,9 @@ class UnitManager:
 		unit.image_path = unit_json["image"]
 		unit.health = unit_json["health"]
 		unit.movement = unit_json["movement"]
+
+		unit.cost_turns = unit_json.get("cost_turns", 4)
+		unit.cost_gold = unit_json.get("cost_gold", unit.cost_turns *3)
 
 		# load maximal amount per player
 		max_per_player = unit_json["max_per_player"]
@@ -99,17 +129,38 @@ class UnitManager:
 	def load_weapon(self, unit_json):
 		weapon_json = unit_json["weapon"]
 		weapon = Weapon()
+
 		weapon.name = weapon_json["name"]
 		weapon.name_de = weapon_json["name_de"]
 		weapon.value = weapon_json["value"]
 		weapon.distance = weapon_json["distance"]
+		weapon.hit_percent = weapon_json.get("hit_percent", 75)
 		weapon.atk_vs_category = weapon_json["atk_vs_category"]
 		# weapon.image_path = weapon_json["image_path"]
 		return weapon
 
+	# get all units as list
 	def get_units(self):
 		return self._units
 
+	# get all unit blueprints as list
+	def get_unit_blueprints(self):
+		skeletons =  []
+		for skeleton in self._skeletons.itervalues():
+			skeletons.append(skeleton)
+		return skeletons
+
+	def get_unit_blueprint(self, blueprint_name):
+		return self._skeletons[blueprint_name]
+
+	# get the names of all unit blueprints as list
+	def get_skeleton_names(self):
+		skeleton_names = []
+		for skeleton in self._skeletons:
+			skeleton_names.append(skeleton.name)
+		return skeleton_names
+
+	# get all units of the player with number player_num as list
 	def get_units_of_player(self, player_num):
 		units_of_player = []
 		for unit in self._units:
@@ -138,11 +189,11 @@ class UnitManager:
 		self._map.units.append(to_spawn)
 		# fire UnitSpawnedEvent
 		ev_unit_spawned = UnitSpawnedEvent(to_spawn, (pos_x, pos_y))
-		self._eventmgr.fire(ev_unit_spawned)
+		EventManager.fire(ev_unit_spawned)
 
 	# move unit to absolute position
 	def move_unit_to(self, unit, (to_x, to_y)):
-		# get relative movement
+		# translate absolute to relative movement
 		rel_x = to_x - unit.pos_x
 		rel_y = to_y - unit.pos_y
 		# weird undocumented movement hacking
@@ -156,35 +207,51 @@ class UnitManager:
 
 	# unit movement relative to own position; unit movement base method
 	def move_unit(self, unit, (rel_x, rel_y)):
-		if unit.mp <= 0:
-			print ("UnitManager: unit " + unit.name + " has no more mp")
+		# check if move is possible
+		move_is_possible = False
+		moves = self.get_possible_moves(unit)
+		target_pos = (unit.pos_x + rel_x, unit.pos_y + rel_y)
+		way = abs(rel_x) + abs(rel_y)
+		if way > unit.mp:
+			print ("[UnitManager] unit " + unit.name + " isn't allowed to move " + str(way) + " tiles")
 			return
-		# restrict two-direction movement, not allowed yet
-		if rel_x != 0 and rel_y != 0:
-			print ("UnitManager: no two-direction movement of units allowed")
-			return
-		# horizontal movement
-		elif rel_x != 0 and rel_y == 0:
-			# check if way is too long
-			way = abs(rel_x)
-			if way > unit.mp:
-				print ("UnitManager: unit " + unit.name + " isn't allowed to move " + str(way) + " tiles")
-				return
-			unit.pos_x += rel_x
-			unit.mp -= abs(rel_x)
-		# vertical movement
-		elif rel_x == 0 and rel_y != 0:
-			# check if way is too long
-			way = abs(rel_y)
-			if way > unit.mp:
-				print ("UnitManager: unit " + unit.name + " isn't allowed to move " + str(way) + " tiles")
-				return
-			unit.pos_y += rel_y
-			unit.mp -= abs(rel_y)
+		for movementtile in moves:
+			if movementtile.get_position() == target_pos:
+				move_is_possible = True
+		# move unit and reduce mp
+		unit.pos_x += rel_x
+		unit.pos_y += rel_y
+		unit.mp -= way
 		#fire UnitMovedEvent
 		ev_unit_moved = UnitMovedEvent(unit)
-		self._eventmgr.fire(ev_unit_moved)
+		EventManager.fire(ev_unit_moved)
 		return unit
+
+	# attack unit
+	def attack_unit(self, attacker, defender):
+		# enough mp?
+		if attacker.mp >= 1:
+			defender_cat = defender.categories[0]
+			attack_damage = attacker.weapon.atk_vs_category.get(defender_cat, 0)
+			if attack_damage == 0:
+				print("[Fight] No damage for category " + defender_cat)
+			# roll dice
+			hit_percent = attacker.weapon.hit_percent
+			rnd_percent = randrange(0, 100, 1)
+			print "[Fight] Roll dice: <" + str(hit_percent) + " needed. got " + str(rnd_percent)
+			# on hit success
+			if (rnd_percent < hit_percent):
+				# deal damage & reduce attacker mp
+				defender.damage(attack_damage)
+				attacker.mp = attacker.mp -1
+				print("[Fight] attacker=" + repr(attacker) + " defender=" + repr(defender) + " damage=" + str(attack_damage))
+				# destroy defender unit when hp <= 0
+				if (defender.hp <= 0):
+					self.remove_unit(defender)
+					print("[Fight] defender died.")
+		# no more mp
+		else:
+			print("[Fight] no more mp.")
 
 	# remove/kill unit
 	def remove_unit(self, unit):
@@ -202,26 +269,57 @@ class UnitManager:
 		movement_helper = UnitMovementHelper(unit, self._map)
 		return movement_helper.get_possible_moves()
 
+	def get_possible_attacks(self, unit):
+		attack_distance = unit.weapon.distance
+		movement_helper = UnitMovementHelper(unit, self._map, ignore_lds=True)
+		# get all tiles the weapon can possibly attack
+		possible_attacks = movement_helper.get_possible_moves(distance=attack_distance)
+		attacks = []
+		for pseudotile in possible_attacks:
+			unit_in_rad = self.get_unit_at((pseudotile.pos_x, pseudotile.pos_y))
+			# when an enemy unit is on the tile -> add to attacks array
+			if unit_in_rad != None and unit_in_rad.player_num != self._actual_player:
+				attacks.append(pseudotile)
+		return attacks
+
 	def on_event(self, event):
 		# on tile selectection: check if a unit is also selected
 		if isinstance(event, TileSelectedEvent):
 			selected_unit = self.get_unit_at(event.pos, for_specific_player=self._actual_player)
 			if selected_unit != None:
-				# save selectec unit in UnitManager
-				self._selected = selected_unit
-				moves = self.get_possible_moves(selected_unit)
-				# selected_unit._possible_moves = moves
-				# fire UnitSelectedEvent
-				ev_selected_unit = UnitSelectedEvent(selected_unit, moves=moves)
-				self._eventmgr.fire(ev_selected_unit)
-			# move unit
+				self.select_unit(selected_unit)
+			# move or attack with unit
 			if selected_unit == None and self._selected != None:
-				print("move?")
-				self.move_unit_to(self._selected, (event.pos))
+				# self._selected is selected unit until unselected
+				moves = self.get_possible_moves(self._selected)
+				attacks = self.get_possible_attacks(self._selected)
+				action = None
+				# selected unit can move to selected tile
+				for moveable_tile in moves:
+					if moveable_tile.get_position() == event.pos:
+						action = "move"
+				# selected unit can attack a unit on selected tile
+				for attackable_tile in attacks:
+					if attackable_tile.get_position() == event.pos:
+						action = "attack"
+						defender = self.get_unit_at(event.pos)
+						attacker = self._selected
+						self.attack_unit(attacker, defender)
+				if action == "move":
+					self.move_unit_to(self._selected, (event.pos))
+
+		# select unit after movement
+		if isinstance(event, UnitMovedEvent):
+			moved_unit = event.unit
+			self.select_unit(moved_unit)
+
+		# cache selected city
+		if isinstance(event, CitySelectedEvent):
+			self._selected_city = event.city
 
 		# when map is loaded
 		if isinstance(event, MapLoadedEvent):
-			# load tilemap
+			# cache loaded tilemap
 			self._map = event.tilemap
 
 		# on right click: unselect actual unit
@@ -230,17 +328,7 @@ class UnitManager:
 				# fire UnitUnselectedEvent
 				self._selected = None
 				ev_unselected_unit = UnitUnselectedEvent()
-				self._eventmgr.fire(ev_unselected_unit)
-
-		# load actualized unit properties and move when it's moved
-		if isinstance(event, UnitMovedEvent):
-			# load new unit and possible moves
-			moved_unit = event.unit
-			moves = self.get_possible_moves(moved_unit)
-			# fire UnitSelectedEvent with updated data
-			ev_selected_unit = UnitSelectedEvent(moved_unit, moves=moves)
-			self._eventmgr.fire(ev_selected_unit)
-
+				EventManager.fire(ev_unselected_unit)
 
 		# reset unit movement points before player starts
 		if isinstance(event, NextOneEvent):
