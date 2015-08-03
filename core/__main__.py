1a7d9a3c41ae7c0ba70eb6c7c94101df576eb12d
herrschr <sysherrsch0r@gmail.com>
2015-08-03 18:01:27 +0200

    main.py: set window configuration

--- core/__main__.py
+++ core/__main__.py
@@ -1,2 +1,7 @@
-from core import app
-app()
+from core.app import PocketLordApp
+from kivy.core.window import Window
+from kivy.config import Config
+
+Config.set("graphics", "width", 1000)
+Config.set("graphics", "height", 800)
+PocketLordApp().run()
