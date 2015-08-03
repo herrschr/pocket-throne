a5ebb15ecd98c2a4aaddcdc9c54e99151f9ef467
herrschr <sysherrsch0r@gmail.com>
2015-08-03 17:37:05 +0200

    tile: added snow and forest tiles; added method to get the neighbor tiles

--- core/entities/tile.py
+++ core/entities/tile.py
@@ -3,32 +3,39 @@
 class Tile:
 	# sys properties
 	_id = -1
+	# neighbor tile
 	_neighbor_west = None
 	_neighbor_north = None
 	_neighbor_east = None
 	_neighbor_south = None
-
-	# tile path & name constants
+	# texture path override
 	image_override = None
+	# default texture
+	texture = None
+	# default texture path
 	image_paths = {
 		"G": "tile_grass.png",
 		"D": "tile_dirt.png",
 		"W": "tile_water.png",
 		"M": "tile_mountains.png",
-		"F": "tile_forest.png"}
+		"F": "tile_forest.png",
+		"S": "tile_snow.png"}
+	# english tile names
 	names = {
 		"G": "Green Grasslands",
 		"D": "Dirty Landscapes",
 		"W": "The Sea",
 		"M": "High Mountains",
-		"F": "A Dark Forest"}
+		"F": "A Dark Forest",
+		"S": "Cold Snow"}
 	names_de = {
 		"G": "Grüne Wiesen",
 		"D": "Dürre Erde",
 		"W": "Wasser",
 		"M": "Hohe Berge",
-		"F": "Düsterer Wald"}
-	walkable = {"G": True, "D": True, "W": False, "M": False, "F": True}
+		"F": "Düsterer Wald",
+		"S": "Eisige Kälte"}
+	walkable = {"G": True, "D": True, "W": False, "M": False, "F": True, "S": True}
 
 	def __init__(self,x,y,landscape=None):
 		self.pos_x = x
@@ -42,6 +49,18 @@ class Tile:
 		if self.image_override:
 			return self.image_override
 		return self.image_paths.get(self.landscape, None)
+
+	def get_neighbor(self, dir_str):
+		if dir_str == "N":
+			return self._neighbor_north
+		elif dir_str == "S":
+			return self._neighbor_south
+		elif dir_str == "W":
+			return self._neighbor_west
+		elif dir_str == "E":
+			return self._neighbor_east
+		else:
+			return None
 
 	def get_name(self):
 		return self.names.get(self.landscape, None)
