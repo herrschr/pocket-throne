f9b22d5b5741bb0253b29c5eccef28ac309c5f72
herrschr <sysherrsch0r@gmail.com>
2015-08-13 16:17:18 +0200

    player: added getters; extended repr with fraction name

--- core/entities/player.py
+++ core/entities/player.py
@@ -6,4 +6,16 @@ class Player(object):
 	fraction = None
 
 	def __repr__(self):
-		return "<Player num=" + str(self.num) + " name=" + self.name + ">"
+		return "<Player num=" + str(self.num) + " name=" + self.name + " fraction=" + repr(self.fraction) + ">"
+
+	def get_number(self):
+		return self.num
+
+	def get_name(self):
+		return self.name
+
+	def get_color(self):
+		return self.color
+
+	def get_fraction(self):
+		return self.fraction
