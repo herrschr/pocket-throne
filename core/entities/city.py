8abe85cd71eeac454d4bf837463996fa0329bd6b
herrschr <sysherrsch0r@gmail.com>
2015-08-13 14:45:05 +0200

    city: add methods to add buildings and build a city wall

--- core/entities/city.py
+++ core/entities/city.py
@@ -5,22 +5,25 @@ from random import randrange
 
 from core.managers.eventmanager import EventManager
 
+#	NNNNN
+#	NOLON
+#	NLOLN
+#	NOLON
+#	NNNNN
+
 class City:
-	# static city vars, defaults
+	# engine properties
 	_id = -1
 	name = ""
 	name_de = ""
+	_map = None
 	image_path = None
 	json_path = None
 	size = "1x1"
 
+	# city flags
 	is_undestroyable = False
 	is_owned_by_nature = False
-
-	has_player_max = False
-	has_map_max = False
-	max_per_player = -1
-	may_per_map = -1
 
 	# changeable building vars
 	playerId = -1
@@ -28,35 +31,52 @@ class City:
 	hp = -1
 	pos_x = -1
 	pos_y = -1
+
+	# buildings
 	buildings = []
 
+	# production
 	production = []
 	production_time = -1
 
-	def __init__(self):
+	def __init__(self, name=None):
 		# register as listener in EventManager
 		EventManager.register_listener(self)
 		# set name & hp
 		self.hp = 30
-		self.name = self.get_random_name()
+		if not name:
+			self.name = self.get_random_name()
+		else:
+			self.name = name
 
 	# generates a random name for the city
 	def get_random_name(self):
-		prefixes = ["Iron", "Green", "Wood", "Dragon", "Dried",  "Old", "New", "Saint", "Death"]
+		prefixes = ["Iron", "Green", "Wood", "Wild", "Dried",  "Old", "New", "Saint", "Death"]
 		postfixes = ["valley", "mountain", "city", "river", "smith",  " Creek", " Towers", " Monument", " Settlement"]
 
 		prefix_rnd = randrange(0, len(prefixes) -1, 1)
 		postfix_rnd = randrange(0, len(postfixes) -1, 1)
 		city_name = prefixes[prefix_rnd] + postfixes[postfix_rnd]
-		print("city name: " + city_name)
 		return city_name
 
 	# get the name of this city
 	def get_name(self):
 		return self.name
 
+	def set_name(self, name):
+		self.name = name
+
 	def get_player_num(self):
 		return self.playerId
+
+	def set_player_num(self, player_num):
+		self.playerId = player_num
+
+	def get_image_path(self):
+		if self.get_size() == 1:
+			return "city_village.png"
+		elif self.get_size() == 2:
+			return "city_city.png"
 
 	# set the position of this city
 	def set_position(self, (pos_x, pos_y)):
@@ -93,10 +113,94 @@ class City:
 
 	# start recruiting a new unit
 	def recruit_unit(self, unit_blueprint):
+		# set production and production time
 		self.production.append(unit_blueprint)
 		self.production_time = unit_blueprint.cost_turns
+		# fire CityRecruitmentStartedEvent
 		ev_recruiting_started = CityRecruitmentStartedEvent(self, unit_blueprint)
 		EventManager.fire(ev_recruiting_started)
+
+	# returns all buildings of this city
+	def get_buildings(self):
+		return self.buildings
+
+	# add a building at city position relative position
+	def add_building_at(self, building_type, (rel_x, rel_y)):
+		# calculate absulute building position
+		abs_pos = (self.pos_x + rel_x, self.pos_y + rel_y)
+		new_bld = Building(self, building_type)
+		new_bld.set_position(abs_pos)
+		new_bld.rel_x = rel_x
+		new_bld.rel_y = rel_y
+		self.buildings.append(new_bld)
+		return new_bld
+
+	# add a building at a random position inside the city
+	def add_building(self, building_type):
+		positions = [(-1,-1), (1,-1), (1,1), (-1,1)]
+		rnd = randrange(0, 3, 1)
+		rel_pos = positions[rnd]
+		self.add_building_at(building_type, rel_pos)
+
+	# build a wall around this city
+	def add_city_wall(self):
+		# add side walls
+		positions = [(-2,-1), (-1,-2), (2,-1), (-1,2)]
+		side = 1
+		while side <= 4:
+			start_pos = positions[side -1]
+			self._add_city_wall_line(side, start_pos)
+			side += 1
+		self._add_city_wall_edges()
+
+	# add a city wall on one side of this town
+	# 1 = left; 2 = top; 3 = right; 4 = bottom
+	def _add_city_wall_line(self, side, (rel_x, rel_y), length=3):
+		# set correct wall images
+		image_path = None
+		gate_image_path = "city_wall_gate_hor.png"
+		vertically = True
+		if side == 1:
+			image_path = "city_wall_left.png"
+		elif side == 2 or side == 4:
+			image_path = "city_wall_hor.png"
+			vertically = False
+		elif side == 3:
+			image_path = "city_wall_right.png"
+		# make a city wall line
+		i = 0
+		while i < length:
+			# get tile at wall position
+			wall = self.add_building_at("wall", (rel_x, rel_y))
+			wall.image_override = image_path
+			if vertically:
+				rel_y += 1
+			else:
+				rel_x += 1
+				# make wall gates on top & bottom
+				if i == 1:
+					wall.image_override = gate_image_path
+			i += 1
+
+	# add the city wall edges to this town
+	def _add_city_wall_edges(self):
+		# city wall: relative edge positions
+		edge_positions = [(-2,-2), (2,-2), (-2,2), (2,2)]
+		# city wall edges: image paths
+		edge_img_prefix = "city_wall_edge_"
+		edge_img_postfix = ["lefttop", "righttop", "leftbottom", "rightbottom"]
+		# make city wall edges
+		i = 0
+		while i < 4:
+			# calculate absolute building position
+			wall_position = edge_positions[i]
+			# build wall wall edges
+			# get full wall edge image path
+			edge_img_path = edge_img_prefix + edge_img_postfix[i] + ".png"
+			# create wall building
+			wall_edge = self.add_building_at("wall", wall_position)
+			wall_edge.image_override = edge_img_path
+			i += 1
 
 	# return the unit thats actually in production in this city
 	def get_unit_in_production(self):
@@ -116,9 +220,12 @@ class City:
 		if self.production_time > -1:
 			self.production_time -= 1
 
+	# finish city production
 	def finish_production(self):
+		# fire CityRecruitmentFinishedEvent
 		ev_production_finished = CityRecruitmentFinishedEvent(self, self.get_unit_in_production())
 		EventManager.fire(ev_production_finished)
+		# clear production unit and time
 		self.production = []
 		self.production_time = -1
 
