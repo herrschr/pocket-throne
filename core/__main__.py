55f92bb6255846d2bf27827ee739b1e3516e0b24
herrschr <sysherrsch0r@gmail.com>
2015-08-13 14:43:08 +0200

    renamed PocketLord in PocketThrone

--- core/__main__.py
+++ core/__main__.py
@@ -1,7 +1,8 @@
-from core.app import PocketLordApp
+from core.app import PocketThroneApp
 from kivy.core.window import Window
 from kivy.config import Config
 
 Config.set("graphics", "width", 1000)
 Config.set("graphics", "height", 800)
-PocketLordApp().run()
+
+PocketThroneApp().run()
--- core/app.py
+++ core/app.py
@@ -30,36 +30,32 @@ from core.gui.sidebar import SideBar
 
 import core.managers
 
-class PocketLordApp(App):
+class PocketThroneApp(App):
 	def build(self):
 		self.initialize_managers()
 		# create MapWidget in Scatter for free movement
 		mapwidget = MapWidget(size=(Locator.TILEMAP.size_x *40, Locator.TILEMAP.size_y *40))
 		# create GUI
 		bottombar = BottomBar()
-		sidebar_scroller = ScrollView(do_scroll_x=False, size_hint=(.2, .8), pos=(0, 100), bar_pos_y="left", bar_width=20)
-		sidebar = SideBar()
-		sidebar_scroller.add_widget(sidebar)
 		# add all to root layout
 		root = FloatLayout(pos=(0,0), size=(Window.size))
 		root.add_widget(mapwidget)
 		root.add_widget(bottombar)
-		root.add_widget(sidebar_scroller)
 
 		# add layouts to GuiManager and return root
 		Locator.GUI_MGR.root = root
 		Locator.GUI_MGR.bottombar = bottombar
-		Locator.GUI_MGR.sidebar = sidebar
 		Locator.GUI_MGR.mapwidget = mapwidget
 		return root
 
 	def initialize_managers(self):
+		# set basic managers in Locator class
 		Locator.INPUT_MGR = InputManager()
 		Locator.GAMELOOP_MGR = GameLoopManager()
 
 		# Manager initialization
 		Locator.MAP_MGR = MapManager(map_name="westeros", mod="westeros")
-		# initialize global managers
+		# initialize global managers in Locator class
 		Locator.TILEMAP = Locator.MAP_MGR.get_loaded_map()
 		Locator.UNIT_MGR = UnitManager(Locator.TILEMAP, mod="westeros")
 		Locator.CITY_MGR = CityManager(Locator.TILEMAP, mod="westeros")
@@ -67,13 +63,13 @@ class PocketLordApp(App):
 		Locator.INGAME_MGR = IngameManager(mod="westeros")
 
 		# add two players
-		Locator.INGAME_MGR.add_new_player("Player 1", (255, 0, 0))
-		Locator.INGAME_MGR.add_new_player("Player 2", (0, 0, 255))
+		Locator.INGAME_MGR.add_new_player("Rebellion", (255, 0, 0))
+		Locator.INGAME_MGR.add_new_player("Loyalists", (0, 0, 255))
 
 		# add some units
-		Locator.UNIT_MGR.spawn_unit_at(1, "soldier", (23, 23))
-		Locator.UNIT_MGR.spawn_unit_at(1, "courier", (23, 24))
-		Locator.UNIT_MGR.spawn_unit_at(2, "crossbowman", (30, 56))
+		Locator.UNIT_MGR.spawn_unit_at(2, "soldier", (23, 23))
+		Locator.UNIT_MGR.spawn_unit_at(2, "courier", (23, 24))
+		Locator.UNIT_MGR.spawn_unit_at(1, "crossbowman", (27, 56))
 
 		# start loop
 		Locator.GAMELOOP_MGR.run()
