75bc18a8c3aee74dee2730e069c2b1a70f2f4f2c
herrschr <sysherrsch0r@gmail.com>
2015-08-13 14:39:05 +0200

    unitmovementhelper: get possible unit movement as a circle

--- core/tools/unitmovementhelper.py
+++ core/tools/unitmovementhelper.py
@@ -17,41 +17,79 @@ class UnitMovementHelper:
 		self.tilemap = tilemap
 		self.ignore_lds = ignore_lds
 
-	def check_direction(self, (from_x, from_y), dir, mp):
-		possible_moves = []
-		while mp > 0:
-			# check towards west: x--
-			if dir == self.DIR_WEST and from_x >= 0:
-				from_x -= 1
-			# check towards north: y--
-			if dir == self.DIR_NORTH and from_y >= 0:
-				from_y -= 1
-			# check towards east: x++
-			if dir == self.DIR_EAST and from_x < self.tilemap.size_x:
-				from_x += 1
-			# check towards south: y++
-			if dir == self.DIR_SOUTH and from_y < self.tilemap.size_y:
-				from_y += 1
-			# check if tile is solid
-			tile_to_check = self.tilemap.get_tile_at((from_x, from_y))
-			if not tile_to_check:
-				return possible_moves
-			if not tile_to_check.is_walkable():
-				if not self.ignore_lds:
-					return possible_moves
-			pseudotile = Tile(from_x, from_y)
-			possible_moves.append(pseudotile)
-			mp -= 1
-		return possible_moves
+	# returns pseudotiles in a circle with a given radius & position
+	def get_tiles_in_circle(self, (from_x, from_y), radius):
+		x = radius
+		y = 0
+		# decision criterion divided by 2 evaluated at x=r, y=0
+		decision_over_2 = 1 - x
+		tiles = []
 
+		while x >= y:
+			#1
+			to_add = Tile(x + from_x,  y + from_y)
+			tiles.append(to_add)
+			#2
+			to_add = Tile(y + from_x,  x + from_y)
+			tiles.append(to_add)
+			#3
+			to_add = Tile(-x + from_x,  y + from_y)
+			tiles.append(to_add)
+			#4
+			to_add = Tile(-y + from_x,  x + from_y)
+			tiles.append(to_add)
+			#5
+			to_add = Tile(-x + from_x, -y + from_y)
+			tiles.append(to_add)
+			#6
+			to_add = Tile(-y + from_x, -x + from_y)
+			tiles.append(to_add)
+			#7
+			to_add = Tile(x + from_x, -y + from_y)
+			tiles.append(to_add)
+			#8
+			to_add = Tile(y + from_x, -x + from_y)
+			tiles.append(to_add)
+
+			y += 1
+			# decision over 2
+			if decision_over_2 <= 0:
+				# change in decision criterion for y -> y+1
+				decision_over_2 += 2 * y + 1
+			else:
+				# change for y: y+1, x -> x-1
+				x -= 1
+				decision_over_2 += 2 * (y - x) + 1
+
+		return tiles
+
+	# check if a tile is walkable by a normal unit
+	def is_walkable_tile(self, pseudotile):
+		tile_to_check = self.tilemap.get_tile_at(pseudotile.get_position())
+		if not tile_to_check:
+			return False
+		if not tile_to_check.is_walkable():
+			return False
+		else:
+			return True
+
+	# get all possible moves in pseudotiles for the unit given in constuctor
 	def get_possible_moves(self, distance=None):
 		moves = []
+		walkable_moves = []
 		unit = self.unit
+
+		# select all tils in filled cirlce
 		if distance == None:
 			distance = self.unit.mp
-		towards_dir = 0
-		while towards_dir < 4:
-			pseudotiles = self.check_direction(unit.get_position(), towards_dir, distance)
-			moves += pseudotiles
-			towards_dir += 1
-		return moves
+			curr_dist = 1
+			while curr_dist <= distance:
+				curr_moves = self.get_tiles_in_circle(unit.get_position(), curr_dist)
+				moves.extend(curr_moves)
+				curr_dist += 1
+
+		# remove not walkable tiles
+		for pseudotile in moves:
+			if self.is_walkable_tile(pseudotile) and not self.ignore_lds:
+				walkable_moves.append(pseudotile)
+		return walkable_moves
