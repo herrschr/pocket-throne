c4f0593a189379e1d4ea051f256792e160c6f2e7
herrschr <sysherrsch0r@gmail.com>
2015-08-13 14:47:18 +0200

    sidebar: remove SideBar on button click (dialog style)

--- core/gui/sidebar.py
+++ core/gui/sidebar.py
@@ -3,6 +3,9 @@ from kivy.uix.scrollview import ScrollView
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 
+from core.entities.event import *
+
+from core.managers.eventmanager import EventManager
 from core.managers.locator import Locator
 
 class SideBar(BoxLayout):
@@ -10,7 +13,8 @@ class SideBar(BoxLayout):
 
 	# initilaize with BoxLayout parameters
 	def __init__(self, **kwargs):
-		super(SideBar, self).__init__(orientation="vertical", padding=10)
+		super(SideBar, self).__init__(orientation="vertical", padding=10, size_hint=(.2, .8), pos=(0, 100))
+		EventManager.register_listener(self)
 
 	# add a widget to SideBar and the buttons list
 	def add_widget(self, widget):
@@ -26,7 +30,10 @@ class SideBar(BoxLayout):
 			buildunitbutton.tag = tag
 			self.add_widget(buildunitbutton)
 
-	# remove anything
-	def clear_widgets(self):
-		for child in self.children[:]:
-			self.remove_widget(child)
+	def remove_self(self):
+		self.parent.remove_widget(self)
+
+	def on_event(self, event):
+		if isinstance(event, GuiButtonClickedEvent):
+			if event.button.parent == self:
+				self.remove_self()
