72ff6e2e149854047fa589fec97382581b4b9611
herrschr <sysherrsch0r@gmail.com>
2015-08-17 23:17:23 +0200

    __main__.py: added out-commented profiling

--- core/__main__.py
+++ core/__main__.py
@@ -1,8 +1,10 @@
 from core.app import PocketThroneApp
 from kivy.core.window import Window
 from kivy.config import Config
+import cProfile
 
 Config.set("graphics", "width", 1000)
 Config.set("graphics", "height", 800)
 
+#cProfile.run('PocketThroneApp().run()')
 PocketThroneApp().run()
