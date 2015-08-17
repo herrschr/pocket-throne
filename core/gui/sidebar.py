36a634f661e5c2ef368472a0389a39afb42f7dab
herrschr <sysherrsch0r@gmail.com>
2015-08-18 00:34:51 +0200

    sidebar: commented

--- core/gui/sidebar.py
+++ core/gui/sidebar.py
@@ -8,12 +8,14 @@ from core.entities.event import *
 from core.managers.eventmanager import EventManager
 from core.managers.locator import Locator
 
+# kivy widget implementation of the SideBar, used for recruiting, build & menu buttons
+# please create it forst and then call a show_* method
 class SideBar(BoxLayout):
 	buttons = []
 
-	# initilaize with BoxLayout parameters
+	# initialize with BoxLayout parameters
 	def __init__(self, **kwargs):
-		super(SideBar, self).__init__(orientation="vertical", padding=10, size_hint=(.2, .8), pos=(0, 100))
+		super(SideBar, self).__init__(orientation="vertical", padding=4, size_hint=(.2, .8), pos=(0, 100))
 		EventManager.register_listener(self)
 
 	# add a widget to SideBar and the buttons list
@@ -21,19 +23,23 @@ class SideBar(BoxLayout):
 		super(SideBar, self).add_widget(widget)
 		self.buttons.append(widget)
 
-	# Show recruitable units when a city is selected
+	# show recruitable units in this sidebar
 	def show_recruitable_units(self, recruitable):
 		for blueprint in recruitable:
+			# show english unit name; set unit type as button tag
 			text = blueprint.get_name()
 			tag = "BUILD-" + blueprint._basename.upper()
-			buildunitbutton = Button(text=text, size_hint=(1, None), size=(100, 40), on_press=Locator.GUI_MGR.button_clicked)
+			# create a button for each unit and add it to sidebar
+			buildunitbutton = Button(text=text, size_hint=(1, None), size=(100, 30), on_press=Locator.GUI_MGR.button_clicked)
 			buildunitbutton.tag = tag
 			self.add_widget(buildunitbutton)
 
+	# method to destroy this SideBar widget
 	def remove_self(self):
 		self.parent.remove_widget(self)
 
 	def on_event(self, event):
+		# when a button is clicked -> destroy the sidebar (dialog style)
 		if isinstance(event, GuiButtonClickedEvent):
 			if event.button.parent == self:
 				self.remove_self()
