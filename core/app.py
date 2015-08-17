1f556e59a1c5c56987c2c686a717db48ffb808a9
herrschr <sysherrsch0r@gmail.com>
2015-08-18 00:31:43 +0200

    app.py: set stark fraction to  rebellion player & lannister fraction to loyalist player

--- core/app.py
+++ core/app.py
@@ -49,29 +49,31 @@ class PocketThroneApp(App):
 		return root
 
 	def initialize_managers(self):
+		# set mod and map to load here
+		_mod_name = "westeros"
+		_map_name = "westeros"
+
 		# set basic managers in Locator class
 		Locator.INPUT_MGR = InputManager()
 		Locator.GAMELOOP_MGR = GameLoopManager()
 
-		# Manager initialization
-		Locator.MAP_MGR = MapManager(map_name="westeros", mod="westeros")
-		# initialize global managers in Locator class
+		# Manager initialization inside Locator holder class
+		Locator.MAP_MGR = MapManager(map_name=_map_name, mod=_mod_name)
 		Locator.TILEMAP = Locator.MAP_MGR.get_loaded_map()
-		Locator.UNIT_MGR = UnitManager(Locator.TILEMAP, mod="westeros")
-		Locator.CITY_MGR = CityManager(Locator.TILEMAP, mod="westeros")
+		Locator.UNIT_MGR = UnitManager(Locator.TILEMAP, mod=_mod_name)
+		Locator.CITY_MGR = CityManager(Locator.TILEMAP, mod=_mod_name)
 		Locator.GUI_MGR = GuiManager()
-		Locator.PLAYER_MGR = PlayerManager(mod="westeros")
+		Locator.PLAYER_MGR = PlayerManager(mod=_mod_name)
 
 		# add two players
-		Locator.PLAYER_MGR.add_new_player("Rebellion", (255, 0, 0))
-		Locator.PLAYER_MGR.add_new_player("Loyalists", (0, 0, 255))
+		Locator.PLAYER_MGR.add_new_player("Rebellion", (255, 0, 0), fraction="stark")
+		Locator.PLAYER_MGR.add_new_player("Loyalists", (0, 0, 255), fraction="lannister")
 
 		# add some units
 		Locator.UNIT_MGR.spawn_unit_at(2, "soldier", (23, 23))
-		Locator.UNIT_MGR.spawn_unit_at(2, "courier", (23, 24))
-		Locator.UNIT_MGR.spawn_unit_at(1, "crossbowman", (27, 56))
+		Locator.UNIT_MGR.spawn_unit_at(1, "archer", (27, 56))
 
-		# start loop
+		# start loop & first turn
 		Locator.GAMELOOP_MGR.run()
 		Locator.PLAYER_MGR.start_game()
 
