5b76538fa392be99d8fe39404a44951125dfe145
herrschr <sysherrsch0r@gmail.com>
2015-10-29 16:22:48 +0100

    entities module: add weapon & region import

--- core/entities/__init__.py
+++ core/entities/__init__.py
@@ -1,9 +1,11 @@
-__all__ = ["building", "city", "event", "player", "tile", "tilemap", "unit"]
+__all__ = ["building", "city", "event", "player", "region", "tile", "tilemap", "unit"]
 
 from building import Building
 from city import City
 from event import *
 from player import Player
+from region import Region
 from tile import Tile
 from tilemap import TileMap
-from unit import Unit, Weapon
+from unit import Unit
+from weapon import Weapon
