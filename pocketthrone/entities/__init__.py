3f538107cce12c6f46a1310a87f7af9468c3bb0f
herrschr <sysherrsch0r@gmail.com>
2015-11-24 13:44:08 +0100

    added weapon and enum entity

---
+++ pocketthrone/entities/__init__.py
@@ -0,0 +1,14 @@
+__all__ = ("building", "city", "event", "player", "region", "tile", "tilemap", "unit", "weapon", "enum")
+
+import __future__
+from event import *
+
+from building import Building
+from city import City
+from player import Player
+from region import Region
+from tile import Tile
+from tilemap import TileMap
+from unit import Unit
+from weapon import Weapon
+from enum import Enum
