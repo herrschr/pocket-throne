b5f4a039bac7c0b79cd954c50d0040d5aa50af38
herrschr <sysherrsch0r@gmail.com>
2016-03-26 18:24:50 +0100

    renamed locator in pipe

---
+++ pocketthrone/managers/pipe.py
@@ -0,0 +1,17 @@
+# class to cache all game manager singletons
+# this will be done in core/app.py
+class L:
+	ModManager = None
+	InputManager = None
+	PlayerManager = None
+	GameLoopManager = None
+	MapManager = None
+	ModManager = None
+	UnitManager = None
+	CityManager = None
+	WidgetManager = None
+
+	TileMap = None
+	Screen = None
+	Grid = None
+	RootDirectory = None
