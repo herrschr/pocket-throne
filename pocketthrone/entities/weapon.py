9165aa255e211aafc4b3ecd1743f21a711c22054
herrschr <sysherrsch0r@gmail.com>
2016-03-26 18:40:24 +0100

    weapon.py: rebalanced

--- pocketthrone/entities/weapon.py
+++ pocketthrone/entities/weapon.py
@@ -7,7 +7,7 @@ class Weapon(object):
 	name_de = ""
 
 	# weapon category (SWORD, SPEAR, BOW)
-	weapon_type = WeaponType(initial=WeaponType.WEAPON_SWORD)
+	category = WeaponType(initial=WeaponType.WEAPON_SWORD)
 	# weapon constants
 	value = 1
 	distance = 1
@@ -21,47 +21,48 @@ class Weapon(object):
 	def __init__(self, weapon_type):
 		self.weapon_type = weapon_type
 
-	# returns the default multiplier vs. an unit category
 	def get_default_multiplier(self, weapon_type):
+		'''returns the default multiplier vs. an unit category'''
 		multiplier = None
 		# SWORD
 		if weapon_type == WeaponType.WEAPON_SWORD:
 			multiplier = {
-				UnitCategory.UNIT_INFANTRY: 1,
-				UnitCategory.UNIT_HEAVYINFANTRY: 0.5,
-				UnitCategory.UNIT_CAVALERY: 0.4,
-				UnitCategory.UNIT_MACHINE: 0.8}
+				UnitCategory.UNITCAT_INFANTRY: 1,
+				UnitCategory.UNITCAT_HEAVYINFANTRY: 0.5,
+				UnitCategory.UNITCAT_CAVALERY: 0.4,
+				UnitCategory.UNITCAT_MACHINE: 0.8}
 		# SPEAR
 		elif weapon_type == WeaponType.WEAPON_SPEAR:
 			multiplier = {
-				UnitCategory.UNIT_INFANTRY: 0.4,
-				UnitCategory.UNIT_HEAVYINFANTRY: 0.6,
-				UnitCategory.UNIT_CAVALERY: 1,
-				UnitCategory.UNIT_MACHINE: 1}
+				UnitCategory.UNITCAT_INFANTRY: 0.4,
+				UnitCategory.UNITCAT_HEAVYINFANTRY: 0.8,
+				UnitCategory.UNITCAT_CAVALERY: 1,
+				UnitCategory.UNITCAT_MACHINE: 1}
 		# BOW
 		elif weapon_type == WeaponType.WEAPON_BOW:
 			multiplier = {
-				UnitCategory.UNIT_INFANTRY: 1,
-				UnitCategory.UNIT_HEAVYINFANTRY: 0.4,
-				UnitCategory.UNIT_CAVALERY: 0.6,
-				UnitCategory.UNIT_MACHINE: 0.2}
+				UnitCategory.UNITCAT_INFANTRY: 1,
+				UnitCategory.UNITCAT_HEAVYINFANTRY: 0.4,
+				UnitCategory.UNITCAT_CAVALERY: 0.6,
+				UnitCategory.UNITCAT_MACHINE: 0.4}
 		return multiplier
 
-	# merge default damage multipliers with weapon specific ones
 	def merge_attack_multipliers(self):
+		'''merges default and specific attack multipliers'''
 		default_multiplier = self.get_default_multiplier(self.category)
 		wpn_multiplier = deepcopy(self.multiplier_vs_category)
 		unit_categories = ["INF", "HEAVY_INF", "CAV", "MACHINE"]
 		# set default dmg multiplier when no specific one is set
 		for i in range(0,3):
 			unit_category = unit_categories[i]
+			default_multiplier = self.get_default_multiplier(self.weapon_type)
 			default_damage = default_multiplier[unit_category]
 			wpn_damage = wpn_multiplier.get(unit_category, None)
 			if not wpn_damage:
 				self.multiplier_vs_category[unit_category] = default_damage
 
-	# returns the calculated weapon damage vs. an unit category
 	def get_damage_vs(self, unit_category):
+		'''returns the calculated weapon damage vs. an unit category'''
 		multiplier = self.multiplier_vs_category[unit_category]
 		damage = multiplier * self.damage
 		return damage
