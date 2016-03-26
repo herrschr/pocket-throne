0d0a7e81df728e02d94e6a5266e91714163d0f03
herrschr <sysherrsch0r@gmail.com>
2016-03-26 18:44:59 +0100

    tile.py: added water bridge and desert tiles

--- pocketthrone/entities/tile.py
+++ pocketthrone/entities/tile.py
@@ -1,28 +1,24 @@
 # -*- coding: utf-8 -*-
+from kivy.core.image import Image as CoreImage
+
+from pocketthrone.entities.enum import TileLandscape, TileBiome, Compass
 
 class Tile:
 	# sys properties
 	_tag = "[Tile] "
 	_id = -1
-	# neighbor tile
-	_neighbor_west = None
-	_neighbor_north = None
-	_neighbor_east = None
-	_neighbor_south = None
-	# default texture
-	texture = None
-	# texture path override
-	image_override = None
-	# entity properties
-	landscape = None
+	name = None
+
 	# default texture path
-	image_paths = {
-		"G": "tile_grass.png",
-		"D": "tile_dirt.png",
-		"W": "tile_water.png",
-		"M": "tile_mountains.png",
-		"F": "tile_forest.png",
-		"S": "tile_snow.png"}
+	texture_names = {
+		"G": "tile_g",
+		"D": "tile_d",
+		"W": "tile_w",
+		"M": "tile_m",
+		"F": "tile_f",
+		"S": "tile_s",
+		"H": "tile_h"}
+
 	# english tile names
 	names = {
 		"G": "Green Grasslands",
@@ -30,55 +26,88 @@ class Tile:
 		"W": "The Sea",
 		"M": "High Mountains",
 		"F": "A Dark Forest",
-		"S": "Cold Snow"}
+		"S": "Cold Snow",
+		"H": "Bridge",
+		"=": "Bridge"}
+
+	# german human readable Tile name descriptions
 	names_de = {
 		"G": "Grüne Wiesen",
 		"D": "Dürre Erde",
 		"W": "Wasser",
 		"M": "Hohe Berge",
 		"F": "Düsterer Wald",
-		"S": "Eisige Kälte"}
+		"S": "Eisige Kälte",
+		"H": "Brücke",
+		"=": "Brücke"}
+
 	walkable = {"G": True, "D": True, "W": False, "M": False, "F": True, "S": True}
 
+	pos_x = -1
+	pos_y = -1
+
 	def __init__(self,x,y,landscape=None):
+		# texture path override
+		self.image_override = None
+		self.texture_name = None
+		self.neighbors = {}
+		self.landscape = None
+		# set position
 		self.pos_x = x
 		self.pos_y = y
+		# set name & landscape
 		self.landscape = landscape
 		self.name = self.get_name()
-		self._image_path = self.get_image_path()
+		# initialize image path
+		self._texture_name = "none.png"
 
-	# returns the tiles sprite path in /img folder
-	def get_image_path(self):
-		if self.image_override:
+	def get_texture_name(self):
+		'''returns the tiles sprite path in /img folder'''
+		lds = unicode(self.landscape)
+		if self.image_override != None:
 			return self.image_override
-		return self.image_paths.get(self.landscape, None)
+		texture_name = "tile_" + unicode.lower(lds)
+		# make texture pat
+		return texture_name
 
-	def get_neighbor(self, dir_str):
-		if dir_str == "N":
-			return self._neighbor_north
-		elif dir_str == "S":
-			return self._neighbor_south
-		elif dir_str == "W":
-			return self._neighbor_west
-		elif dir_str == "E":
-			return self._neighbor_east
-		else:
+	def get_neighbor(self, compass):
+		'''returns the neighbor Tile in Compass direction; else returns None'''
+		try:
+			return self.neighbors[compass]
+		except Exception:
 			return None
 
 	def get_name(self):
+		'''returns English name of this Tile\'s landscape'''
 		return self.names.get(self.landscape, None)
 
 	def get_name_de(self):
+		'''returns German name of this Tile\'s landscape'''
 		return self.names_de.get(self.landscape, None)
 
 	def is_walkable(self):
+		'''returns if tile is walkable by land units'''
 		return self.walkable.get(self.landscape, True)
 
 	def get_landscape(self):
+		'''returns landscape of this tile'''
 		return self.landscape
 
+	def set_landscape(self, lds):
+		self.landscape = lds
+
 	def get_position(self):
+		'''returns position of this tile'''
 		return (self.pos_x, self.pos_y)
 
+	def get_x(self):
+		'''returns horizontal position of this tile'''
+		return self.pos_x
+
+	def get_y(self):
+		'''returns vertical position of this tile'''
+		return self.pos_y
+
 	def __repr__(self):
+		'''returns xml like representation of this tile'''
 		return "<Tile lds=" + self.landscape + " pos=" + str(self.get_position()) + ">"
